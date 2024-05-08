2f3950373d148f0253fdbd08c4882ecab77a6c18
Omar <omar@aviamedia.com>
2024-05-08 12:29:43 +0400

    Redirect to Edit Page if user has an application already

--- app/Filament/Resources/ApplicationResource/Pages/CreateApplication.php
+++ app/Filament/Resources/ApplicationResource/Pages/CreateApplication.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\ApplicationResource\Pages;
 
 use App\Filament\Resources\ApplicationResource;
+use App\Filament\Resources\ProgramResource;
+use App\Models\Application;
 use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
@@ -17,12 +19,30 @@ class CreateApplication extends CreateRecord
         return 'Submit Application';
     }
 
-    protected function getCreateFormAction(): Action
+    public function getCreateFormAction(): Action
     {
         return Action::make('create')
             ->label('Submit')
             ->submit('create')
             ->keyBindings(['mod+s']);
+    }
+
+
+    protected function authorizeAccess(): void
+    {
+
+        $program_id = request('program');
+
+        if ( ! $program_id) {
+            $this->redirect(ProgramResource::getUrl('index'));
+        }
+
+        $application = Application::where('user_id', auth()->id())->where('program_id', $program_id)->first();
+
+        // If user has an application with this program, redirect to edit page
+        if ($application) {
+            $this->redirect(ApplicationResource::getUrl('edit', [$application]));
+        }
     }
 
 }
