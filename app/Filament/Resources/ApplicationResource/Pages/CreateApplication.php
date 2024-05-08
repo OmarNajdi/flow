cbbe116545be98da2701fdaff547b6b1c213e2ed
Omar <omar@aviamedia.com>
2024-05-08 09:48:57 +0400

    Update Application Submit Label

--- app/Filament/Resources/ApplicationResource/Pages/CreateApplication.php
+++ app/Filament/Resources/ApplicationResource/Pages/CreateApplication.php
@@ -3,7 +3,7 @@
 namespace App\Filament\Resources\ApplicationResource\Pages;
 
 use App\Filament\Resources\ApplicationResource;
-use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateApplication extends CreateRecord
@@ -11,5 +11,18 @@ class CreateApplication extends CreateRecord
     protected static string $resource = ApplicationResource::class;
 
     protected static bool $canCreateAnother = false;
+
+    public static function getCreateLabel(): string
+    {
+        return 'Submit Application';
+    }
+
+    protected function getCreateFormAction(): Action
+    {
+        return Action::make('create')
+            ->label('Submit')
+            ->submit('create')
+            ->keyBindings(['mod+s']);
+    }
 
 }
