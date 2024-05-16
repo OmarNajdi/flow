de3b9476cd383ed0a22acddc5863223438392bbd
Omar <omar@aviamedia.com>
2024-05-16 15:11:29 +0300

    Update Widget Title

--- resources/views/filament/widgets/apply_widget.blade.php
+++ resources/views/filament/widgets/apply_widget.blade.php
@@ -3,7 +3,7 @@
         <div class="flex items-center gap-x-3">
             <div class="flex-1">
                 <h2 class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white mb-4">
-                    Apply Now
+                    Open Programs
                 </h2>
 
                 <div class="grid grid-cols-3">
