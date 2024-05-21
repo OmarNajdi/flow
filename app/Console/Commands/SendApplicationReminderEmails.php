92d76f8ff61b524ac31cbec6c7fed07c248582bb
Omar <omar@aviamedia.com>
2024-05-21 23:03:59 +0300

    Create Programs Reminder Email

---
+++ app/Console/Commands/SendApplicationReminderEmails.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use App\Notifications\ApplicationReminder;
+use Illuminate\Console\Command;
+
+class SendApplicationReminderEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'application:reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send Application Reminders to Applicants';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $users = User::whereNotIn('id', function ($query) {
+            $query->select('user_id')
+                ->from('applications')
+                ->where('status', 'Submitted');
+        })->get();
+
+        foreach ($users as $user) {
+            $user->notify(new ApplicationReminder([
+                'program'    => 'PIEC',
+                'first_name' => $user->first_name,
+                'close_date' => 'May 22',
+            ]));
+        }
+    }
+}
---
+++ app/Notifications/ApplicationReminder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ApplicationReminder extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected $program, $first_name, $close_date;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($application)
+    {
+        $this->program    = $application['program'];
+        $this->first_name = $application['first_name'];
+        $this->close_date = $application['close_date'];
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Complete Your Application for  '.$this->program.' by '.$this->close_date)
+            ->greeting('Dear '.$this->first_name.',')
+            ->line('Our records indicate that you have started your application to join '.$this->program.' , but it has not yet been submitted. We wanted to remind you that the deadline to finalize and submit your application is fast approaching.')
+            ->line('Closing Date: '.$this->close_date)
+            ->line('To complete your application, please log in to your account and ensure all required information is filled out and submitted. If you encounter any issues or have any questions, please do not hesitate to reach out to our Program team by replying directly to this email.')
+            ->line('This program offers a unique opportunity, and we don’t want you to miss out. We are excited about the potential of having you join us and look forward to reviewing your completed application.')
+            ->line('Thank you for your interest in '.$this->program.'. We wish you the best of luck in completing your application.')
+            ->line('Best regards,')
+            ->line('Flow Accelerator')
+            ->salutation(' ');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
