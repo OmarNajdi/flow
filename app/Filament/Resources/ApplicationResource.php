b8f6d9058e0fb29180ddcfc59cc6506eb14f75c4
Omar <omar@aviamedia.com>
2024-07-09 15:34:38 +0300

    Application Delete Action

--- app/Filament/Resources/ApplicationResource.php
+++ app/Filament/Resources/ApplicationResource.php
@@ -556,6 +556,7 @@ class ApplicationResource extends Resource
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->recordUrl(function ($record) {
                 if ($record->trashed()) {
