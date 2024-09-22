cc904c96837ba34d98d634172a77a8add4a0e80c
Omar <omar@aviamedia.com>
2024-09-22 18:09:05 +0300

    Add Fintech Widget

--- resources/views/filament/widgets/apply_widget.blade.php
+++ resources/views/filament/widgets/apply_widget.blade.php
@@ -7,11 +7,11 @@
                 </h2>
 
                 <div class="grid grid-cols-3 gap-10">
-                    <a href="{{ url('/programs/5') }}">
+                    <a href="{{ url('/programs/7') }}">
                         <div class="bg-gray-100 dark:bg-gray-800 rounded-2xl hover:scale-105 transition-all border">
-                            <img src="{{ asset('images/OC.webp') }}" alt="Apply Now" class="rounded-2xl"/>
-                            <h2 class="text-center pt-2 pb-4 color-primary">{{__('Level: Pre-Acceleration')}}</h2>
-                            <h3 class="font-bold text-2xl text-center pb-6 color-primary">{{__('Program: Orange Corners')}}</h3>
+                            <img src="{{ asset('images/Fintech.webp') }}" alt="Apply Now" class="rounded-2xl"/>
+                            <h2 class="text-center pt-2 pb-4 color-primary">{{__('Level: Ideation And Innovation')}}</h2>
+                            <h3 class="font-bold text-2xl text-center pb-6 color-primary">{{__('Program: PIEC')}}</h3>
                             <button
                                 class="w-full bg-[#01786C] fi-btn grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-2xl fi-color-primary fi-btn-color-primary fi-color-primary fi-size-xl fi-btn-size-xl gap-1.5 px-4 py-3 text-sm inline-grid shadow-sm text-white fi-ac-action fi-ac-btn-action">
                                 <span class="fi-btn-label">{{__('Apply')}}</span>
